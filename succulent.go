f107f6553f4c7fe0477772b4f67975b1b2b4c79e
Ryan James Spencer <spencer.ryanjames@gmail.com>
2022-03-31 20:58:14 +1100

    Outfit toy server

--- succulent.go
+++ succulent.go
@@ -2,6 +2,8 @@ package main
 
 import (
 	"fmt"
+  "net/http"
+  "log"
 )
 
 const MaxUint = ^uint(0)
@@ -10,7 +12,14 @@ const MaxInt = int(MaxUint >> 1)
 const MinInt = -MaxInt - 1
 
 func main() {
-	fmt.Printf("Alas %v\n", 1)
+    http.HandleFunc("/echo", func(w http.ResponseWriter, r *http.Request) {
+        fmt.Fprintf(w, "works")
+    })
+
+    fmt.Printf("Starting server at port 8080\n")
+    if err := http.ListenAndServe(":8080", nil); err != nil {
+        log.Fatal(err)
+    }
 }
 
 func Thing(x bool) bool {
@@ -18,19 +27,19 @@ func Thing(x bool) bool {
 }
 
 func Even(x int) int {
-	return 2
-	//if x % 2 == 1 {
-	//    if x == MinInt {
-	//        return x + 1
-	//    }
-	//    return x - 1
-	//}
-	//return x
+    return 2
+    //if x % 2 == 1 {
+    //    if x == MinInt {
+    //        return x + 1
+    //    }
+    //    return x - 1
+    //}
+    //return x
 }
 
 func MaybePanic(x int) int {
-	//if x % 3 == 0 {
-	//    panic("uh oh")
-	//}
-	return x
+    //if x % 3 == 0 {
+    //    panic("uh oh")
+    //}
+    return x
 }
